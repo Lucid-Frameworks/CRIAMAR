66b0c304d522ccfb2a8ff8dea9598f2fefc10c35
Johannes Becker <git.acct02@outlook.com>
2025-02-15 21:21:29 +0800

    feat(postcss): added postcss-preset-env for modern CSS and cssnano for minification

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,8 @@ module.exports = {
       autoprefixer: {},
       "postcss-nested": {}, 
       "postcss-import": {},
-      "postcss-flexbugs-fixes": {} // Fixes known flexbox issues
+      "postcss-flexbugs-fixes": {}, // Fixes known flexbox issues
+      "postcss-preset-env": {}, // Enables modern CSS features
+      "cssnano": process.env.NODE_ENV === "production" ? {} : false // Minifies CSS in production
     },
 };
