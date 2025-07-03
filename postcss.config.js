c997409a430f76e8b29b8ee536d213bebb58ce41
Johannes Becker <git.acct02@outlook.com>
2025-07-03 12:01:20 +0800

    feat(postcss): enable stage 1 features in postcss-preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -5,13 +5,15 @@ module.exports = {
     tailwindcss: {},
 
     // Optimization & compatibility
-    cssnano: process.env.NODE_ENV === "production" ? {} : false, // Minifies CSS
+    cssnano: process.env.NODE_ENV === "production" ? {} : false,
     "postcss-color-function": process.env.NODE_ENV !== "production" ? {} : false,
     "postcss-custom-properties": {},
     "postcss-discard-comments": process.env.NODE_ENV === "production" ? {} : false,
     "postcss-flexbugs-fixes": {},
     "postcss-logical": {},
-    "postcss-preset-env": {},
+    "postcss-preset-env": {
+      stage: 1, // Use stage 1 features for broader compatibility
+    },
 
     // Features & syntax sugar
     "postcss-extend-rule": {},
